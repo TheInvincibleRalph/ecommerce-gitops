c59b7d8c0b721d8b6c65c7784f7c165ed932976a
Raphael <adesegunraphaeladeoye@gmail.com>
2026-06-22 16:38:05 +0100

    feat: add env

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -123,6 +123,15 @@ resource "aws_ecr_repository" "monolith" {
   force_delete         = true
 }
 
+resource "aws_ecr_repository" "database_repo" {
+  name                 = "database"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 # 2. The OIDC Provider (Allows GitHub to talk to AWS)
 module "iam_github_oidc_provider" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
