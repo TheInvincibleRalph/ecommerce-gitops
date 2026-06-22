793082cf3fa1390308d817d822cb38ad2e13f5b3
Raphael <adesegunraphaeladeoye@gmail.com>
2026-06-22 17:56:55 +0100

    feat: add storage

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -152,3 +152,27 @@ module "iam_github_oidc_role" {
     ECRPush = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
   }
 }
+
+
+# IAM Role for the EBS CSI Driver
+module "ebs_csi_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name             = "ebs-csi-role"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
+# EKS Add-on
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
+}
